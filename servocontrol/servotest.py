a1430b8eae3e3e49b0f34e0a48eb90338964a520
makiladamsuka <makiladamsuka09@gmail.com>
2025-11-07 05:23:02 +0530

    Added servo control and rename some files

---
+++ servocontrol/servotest.py
@@ -0,0 +1,34 @@
+from adafruit_servokit import ServoKit
+import time
+
+try:
+    kit = ServoKit(channels=16) 
+except ValueError:
+    print("ERROR: Could not initialize ServoKit. Check I2C wiring and external power.")
+
+
+PAN_CHANNEL = 0  
+TILT_CHANNEL = 1 
+
+kit.servo[PAN_CHANNEL].set_pulse_width_range(min_pulse=450, max_pulse=2600)
+kit.servo[TILT_CHANNEL].set_pulse_width_range(min_pulse=450, max_pulse=2600)
+
+def move_servos(pan_angle, tilt_angle):
+    kit.servo[PAN_CHANNEL].angle = pan_angle
+    kit.servo[TILT_CHANNEL].angle = tilt_angle
+    print(f"Set Pan: {pan_angle}°, Tilt: {tilt_angle}°")
+
+
+def main():
+    while True:
+        user_input = input("Enter pan angle and tilt angle (e.g., 90 45): ")
+        pan_str, tilt_str = user_input.split()
+
+        pan_angle = int(pan_str)
+        tilt_angle = int(tilt_str)
+        
+        move_servos(pan_angle, tilt_angle)
+
+
+if __name__ == '__main__':
+    main()
